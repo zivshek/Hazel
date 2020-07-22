819acab7808fc67c5462b94f31fc3ed2c3841702
ziv <ziv.hao.shek@gmail.com>
2020-07-22 18:54:44 -0400

    Initial setup

---
+++ Hazel/src/Hazel/EntryPoint.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifdef HZ_PLATFORM_WINDOWS
+
+extern Hazel::Application* Hazel::CreateApplication();
+
+int main(int argc, char** argv)
+{
+	Hazel::Application* app = Hazel::CreateApplication();
+	app->Run();
+	delete app;
+}
+
+#else
+#error Hazel only supports Windows
+#endif
---
+++ Sandbox/src/Sandbox.cpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <Hazel.h>
+
+class Sandbox : public Hazel::Application
+{
+public:
+	Sandbox() {};
+	~Sandbox() {};
+};
+
+Hazel::Application* Hazel::CreateApplication()
+{
+	return new Sandbox();
+}
