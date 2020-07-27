5e3cf0e415802658722a582dce711d066ea2431b
ziv <ziv.hao.shek@gmail.com>
2020-07-26 22:05:10 -0400

    Added premake script to generate solution

---
+++ generate_sln.bat
@@ -0,0 +1,5 @@
+del "%CD%"\Hazel.sln
+del "%CD%"\Hazel\*.vcxproj*
+del "%CD%"\Sandbox\*.vcxproj*
+
+call "%CD%"\vendor\premake\premake5.exe vs2019
---
+++ premake5.lua
@@ -0,0 +1,95 @@
+workspace "Hazel"
+    architecture "x64"
+    configurations {"Debug", "Release", "Dist"}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Hazel"
+    location "Hazel"
+    kind "SharedLib"
+    language "C++"
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:Windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+        defines
+        {
+            "HZ_PLATFORM_WINDOWS", "HZ_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
+        }
+
+    filter "configurations:Debug"
+        defines "HZ_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "DIST"
+        optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "Hazel/vendor/spdlog/include",
+        "Hazel/src"
+    }
+
+    links
+    {
+        "Hazel"
+    }
+
+    filter "system:Windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+        defines
+        {
+            "HZ_PLATFORM_WINDOWS"
+        }
+        
+    filter "configurations:Debug"
+        defines "HZ_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "DIST"
+        optimize "On"
+
