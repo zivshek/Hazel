11203e2616b6a24e58d02e38c2f849a5ee758b45
ziv <ziv.hao.shek@gmail.com>
2020-08-25 22:01:51 -0400

    Fix an error in WindowsInput

--- Hazel/src/Platform/Input/WindowsInput.cpp
+++ Hazel/src/Platform/Input/WindowsInput.cpp
@@ -11,7 +11,7 @@ namespace Hazel
     {
         auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
         auto state = glfwGetKey(window, keycode);
-        return state == GLFW_PRESS || GLFW_REPEAT;
+        return state == GLFW_PRESS || state == GLFW_REPEAT;
     }
 
     bool WindowsInput::IsMouseButtonPressed(int button) const
